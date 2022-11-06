43805ca03ae41387c6b55589ca160d829e90064c
Oleksandra-Rud <sandyrud.com@gmail.com>
2022-11-06 12:45:40 +0200

    fix: change names of the days

--- script.js
+++ script.js
@@ -105,15 +105,7 @@ function displayForecast(response) {
 function formatForecastDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
